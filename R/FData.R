44b26d54d92caa3aec0e9ab6efb1b2e2b3e1090d
Florian Lecocq <florian.lecocq@outlook.com>
2019-05-22 13:35:05 +0200

    Change the type of field X in FData class

--- R/FData.R
+++ R/FData.R
@@ -1,32 +1,26 @@
 FData <- setRefClass(
-  # Set the name for the class
   "FData",
-
-  # Define the fields
   fields = list(
-    X = "matrix",
-    # covariates
-    Y = "matrix",
-    # series
+    X = "numeric", # Covariates
+    Y = "matrix", # Response
     m = "numeric",
     n = "numeric",
     vecY = "matrix"
   ),
-
-  # Set the methods
   methods = list(
-    # set Data from a file
     setData = function(X, Y) {
+
       Y <<- as.matrix(Y)
+
       setDataProperties()
+
       if (n == 1) {
         Y <<- t(Y)
       }
 
-      X <<- as.matrix(X)
+      X <<- X
 
     },
-
 
     setDataProperties = function() {
       n <<- nrow(Y)
